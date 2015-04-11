3aa49f92ef02fe0d82a4938184ead3d1c2a42a61
Jesús Herrera <jesusmaherrera@gmail.com>
2015-04-10 21:56:44 -0600

    Adding endpoints for our User models

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -2,6 +2,7 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
 
+from snippets.serializer import UserSerializer
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
@@ -11,3 +12,11 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+
+class UserDetail(generics.RetriveAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
